81deae1afa138103840b0c94ae5c0209890d08a7
ebruyassi <ebruyassi@gmail.com>
2022-07-06 14:31:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        int countb=0,countl=0;
+        List<int> arr=new List<int>();
+        
+        for(int i=0;i<a.Count;i++){
+            if(a[i]>b[i]){
+                countb++;
+            }else if(a[i]<b[i]){
+                countl++;
+            }    
+        }
+        arr.Add(countb);
+        arr.Add(countl);
+        return arr;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
